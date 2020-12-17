0bb4e0de35f5a0c7087dca770474d71b5d1cf7d0
Audrey Jacobs <audrey.jacobs1999@gmail.com>
2020-12-17 16:00:19 -0500

    bar chart

--- final-budget/src/BudgetPage/BudgetPage.js
+++ final-budget/src/BudgetPage/BudgetPage.js
@@ -1,34 +1,78 @@
 import React, { Component } from 'react';
-// import { Pie } from 'react-chartjs-2'; 
-// import axios from 'axios'; 
+import {Bar, Line, Pie} from 'react-chartjs-2'; 
+// import axios from 'axios';
+var name = window.prompt("name your budget");
 
-function BudgetPage() {
+var chartLabel1 = window.prompt("enter Label 1/3");
+var chartLabel2 = window.prompt("enter Label 2/3");
+var chartLabel3 = window.prompt("enter Label 3/3");
+  
+var chartval1 = window.prompt("enter value 1/3");
+var chartval2 = window.prompt("enter value 2/3");
+var chartval3 = window.prompt("enter value 3/3");
+  
+class BudgetPage extends Component {
 
-  return (
-    <div >
-          <div className="container center">
-                  <h2>My Budget</h2>
-                  
-                  <p>Let's build your budget</p>
+  constructor (props){
+    super(props);
+    this.state = {
+      chartData: {
+            labels: [chartLabel1, chartLabel2 ,chartLabel3], 
+            datasets: [
+                  {
+                    label: name, 
+                    data: [chartval1, chartval2, chartval3], 
+                    backgroundColor: [
+                      'rgba(255, 159, 64, 0.6)', 
+                      'rgba(153, 102, 255, 0.6)', 
+                      'rgba(255, 159, 64, 0.6)', 
+                    ]
+                  }
+              ]
+      }
+    } 
 
-                  <div className="text-box">
-                          <h1>Chart</h1>
-                          <p>
-                              <canvas id="myChart" width="400" height="400"></canvas>
-                          </p>
-                  </div> 
+  }
+    render(){
+      return(
+        <div className="contianer center">
+            <h2>My Budget</h2>
+            <p>Hover over each bar to see label information and value</p>    
+            <div className="text-box">
+                  <Bar 
+                        data ={this.state.chartData}
+                        options={{}}/>
+            </div>
+
+        </div>
+      )
+    }
+
+} export default BudgetPage;  
+// function BudgetPage() {
+
+//   return (
+//     <div >
+//           <div className="container center">
+//                   
+//                   <div className="text-box">
+//                           <h1>Chart</h1>
+//                           <p>
+//                               <canvas id="myChart" width="400" height="400"></canvas>
+//                           </p>
+//                   </div> 
             
-                  <div>
-                        {/* <Pie data={this.state.data} /> */}
-                  </div> 
+//                   <div>
+//                         <Pie data={this.state.data} />
+//                   </div> 
     
-          </div>
-    </div>
+//           </div>
+//     </div>
 
-  );
-}
+//   );
+// }
 
-export default BudgetPage;
+// export default BudgetPage;
 
 
 //   state = {
